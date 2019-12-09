d0d98e5b008020fee4f6fb00fe96f6fdd424399e
chaijewon <vcandjava@nate.com>
2019-12-09 17:34:19 +0900

    2019-12-09-commit3

--- 20191209-데이터제어(제어문-선택조건문)/src/오늘의문제3.java
+++ 20191209-데이터제어(제어문-선택조건문)/src/오늘의문제3.java
@@ -12,11 +12,57 @@
  *              이하 => 'F'
  *    
  */
+import java.util.Scanner;
 public class 오늘의문제3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner scan=new Scanner(System.in);
+        System.out.print("정수 입력:");
+        int num=scan.nextInt();
+        
+        if(num%2==0)
+        	System.out.println("짝수");
+        else
+        	System.out.println("홀수");
+        
+        char c='A';
+        if(c>='A' && c<='Z')
+        	System.out.println("대문자");
+        else
+        	System.out.println("소문자");
+        
+        System.out.print("국어 점수:");
+        int kor=scan.nextInt();
+        
+        System.out.print("영어 점수:");
+        int eng=scan.nextInt();
+        
+        System.out.print("수학 점수:");
+        int math=scan.nextInt();
+        
+        int avg=(kor+eng+math)/3;
+        
+        // 80+90+80 ==> 250/3 ==> 83...
+        
+        int res=avg/10; // 8
+        
+        if(res==10 || res==9)
+        	System.out.println("A");
+        if(res==8)
+        	System.out.println("B");
+        if(res==7)
+        	System.out.println("C");
+        if(res==6)
+        	System.out.println("D");
+        if(res<6)
+        	System.out.println("F");
+        
 	}
 
 }
+
+
+
+
+
