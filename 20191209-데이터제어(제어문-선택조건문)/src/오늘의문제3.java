935b3e6cf0e03c2ab26e0a861cc7312df7c29692
chaijewon <vcandjava@nate.com>
2019-12-09 17:09:19 +0900

    2019-12-09 - commit 2

---
+++ 20191209-데이터제어(제어문-선택조건문)/src/오늘의문제3.java
@@ -0,0 +1,22 @@
+/*
+ *    1. if ~ else 
+ *       정수를 한개 입력받아서 짝수/홀수 
+ *    2. 임의의 알파벳을 받아서 대문자/소문자 (if~else)
+ *       =====
+ *       char a='A'
+ *    3. 정수 3개를 입력을 받아서 
+ *        평균 => 90이상 => 'A'
+ *              80이상 => 'B'
+ *              70이상=> 'C'
+ *              60이상 => 'D'
+ *              이하 => 'F'
+ *    
+ */
+public class 오늘의문제3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
